9bfd8b03e9e56894b4c6132967595ab267d5bc50
Dhika Rizky <deka_r@outlook.com>
2025-10-02 13:45:24 +0700

    Optimizations

--- app/api/generate/route.tsx
+++ app/api/generate/route.tsx
@@ -1,21 +1,6 @@
 import { ImageResponse } from 'next/og'
 import BackgroundImage from './background'
 
-async function loadGoogleFont(font: string, text: string) {
-  const url = `https://fonts.googleapis.com/css2?family=${font}:wght@600&text=${encodeURIComponent(text)}`
-  const css = await (await fetch(url)).text()
-  const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/)
-
-  if (resource) {
-    const response = await fetch(resource[1])
-    if (response.status == 200) {
-      return await response.arrayBuffer()
-    }
-  }
-
-  throw new Error('failed to load font data')
-}
-
 async function fetchArticleData(url: string) {
   let title = 'Something Went Wrong'
   let imageUrl = 'https://http.cat/500'
@@ -55,65 +40,72 @@ async function fetchArticleData(url: string) {
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const articleUrl = searchParams.get('url')
-  let title = 'Please Provide URL'
-  let image = 'https://http.cat/400'
 
+  if (!articleUrl) {
+    return new Response('Missing url parameter, supply ?url=', { status: 400 })
+  }
 
-  if (articleUrl) {
-    const articleData = await fetchArticleData(articleUrl)
-    title = articleData.title
-    image = articleData.imageUrl
+  let title = 'Something Went Wrong'
+  let image = 'https://http.cat/500'
+  let fontData = new ArrayBuffer()
+
+  try {
+    const [
+      fontResponse, articleData
+    ] = await Promise.allSettled([
+      fetch(
+        new URL('/fonts/Figtree-SemiBold.ttf', request.url)
+      ),
+      fetchArticleData(articleUrl)
+    ])
+
+    if (fontResponse.status === 'fulfilled' && articleData.status === 'fulfilled') {
+      fontData = await fontResponse.value.arrayBuffer()
+      title = articleData.value.title
+      image = articleData.value.imageUrl
+    }
+  } catch (err) {
+    return new Response('Failed to fetch article data or fonts', { status: 500 })
   }
 
   return new ImageResponse(
     (
-      <div style={{
-        display: 'flex'
-      }}>
-        <BackgroundImage style={{ position: 'absolute', left: 0, top: 0, zIndex: '1' }} />
-        <img
-          src={image}
-          style={{
-            display: 'block',
-            zIndex: '2',
-            position: 'absolute',
-            left: 85,
-            top: 270,
-            width: 1341,
-            height: 1008,
-            objectFit: 'cover',
-            borderTopLeftRadius: 28,
-            borderTopRightRadius: 28
-          }}
-        />
+      <div style={{ display: 'flex', position: 'relative', width: '100%', height: '100%' }}>
+        <BackgroundImage style={{ position: 'absolute', left: 0, top: 0, zIndex: '1', width: '100%', height: '100%' }} />
+        <img src={image} style={{
+          display: 'block',
+          zIndex: '2',
+          position: 'absolute',
+          left: 32,
+          top: 103,
+          width: 511,
+          height: 384,
+          objectFit: 'cover',
+          borderTopLeftRadius: 11,
+          borderTopRightRadius: 11,
+        }} />
         <div style={{
           display: 'flex',
           zIndex: '2',
           position: 'absolute',
-          left: 83,
-          top: 1270,
+          left: 32,
+          top: 483,
           justifyContent: 'center',
           alignItems: 'center',
           textAlign: 'center',
-          width: 1344,
-          height: 338,
+          width: 512,
+          height: 129,
           color: '#0220B6',
-          padding: '16px 32px'
+          padding: '6px 12px'
         }}>
-          <h1 style={{ fontSize: 64, fontWeight: 600, letterSpacing: '2%', fontFamily: 'Figtree' }}>{title}</h1>
+          <h1 style={{ fontSize: 24, fontWeight: 600, letterSpacing: '2%', fontFamily: 'Figtree' }}>{title}</h1>
         </div>
       </div>
     ),
     {
-      width: 1512,
-      height: 1890,
-      fonts: [
-        {
-          name: 'Figtree',
-          data: await loadGoogleFont('Figtree', title),
-          style: 'normal',
-        }
-      ]
+      width: 576,
+      height: 720,
+      fonts: [{ name: 'Figtree', data: fontData, style: 'normal', weight: 600 }]
     },
   )
 }
